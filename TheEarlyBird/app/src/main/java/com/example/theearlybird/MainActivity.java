7fa3b4ae2c4c7a84999a1f82d5c3d1f679336a92
Bheki Maenetja <bhekimaenetja@gmail.com>
2022-04-29 15:32:48 +0200

    implemented basic functionality to filter news by category

--- TheEarlyBird/app/src/main/java/com/example/theearlybird/MainActivity.java
+++ TheEarlyBird/app/src/main/java/com/example/theearlybird/MainActivity.java
@@ -7,16 +7,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.theearlybird.Models.Article;
 import com.example.theearlybird.Models.NewsApiResponse;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SelectListener {
+public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener {
     RecyclerView recyclerView;
     CustomAdapter adapter;
     ProgressDialog dialog;
+    Button b1, b2,b3,b4,b5,b6,b7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,21 @@ public class MainActivity extends AppCompatActivity implements SelectListener {
         dialog = new ProgressDialog(this);
         dialog.setTitle("Loading news articles");
         dialog.show();
+
+        b1 = findViewById(R.id.btn_1);
+        b1.setOnClickListener(this);
+        b2 = findViewById(R.id.btn_2);
+        b2.setOnClickListener(this);
+        b3 = findViewById(R.id.btn_3);
+        b3.setOnClickListener(this);
+        b4 = findViewById(R.id.btn_4);
+        b4.setOnClickListener(this);
+        b5 = findViewById(R.id.btn_5);
+        b5.setOnClickListener(this);
+        b6 = findViewById(R.id.btn_6);
+        b6.setOnClickListener(this);
+        b7 = findViewById(R.id.btn_7);
+        b7.setOnClickListener(this);
 
         RequestManager manager = new RequestManager(this);
         manager.getNewsHeadlines(listener, "general", null);
@@ -56,5 +74,15 @@ public class MainActivity extends AppCompatActivity implements SelectListener {
     public void OnNewsClicked(Article headlines) {
         startActivity(new Intent(MainActivity.this, DetailsActivity.class)
         .putExtra("data", headlines));
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button button = (Button) v;
+        String category = button.getText().toString();
+        dialog.setTitle("Loading " + category + " articles");
+        dialog.show();
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadlines(listener, category, null);
     }
 }
