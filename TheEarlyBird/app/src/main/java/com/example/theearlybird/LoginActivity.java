d245e7638fb0b3d4ea80482ce0a4948430a8e8de
Bheki Maenetja <bhekimaenetja@gmail.com>
2022-05-04 18:34:46 +0100

    Created login and registration pages

---
+++ TheEarlyBird/app/src/main/java/com/example/theearlybird/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.theearlybird;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText email, password;
+    private Button btnLogin;
+    private TextView textRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.login_email);
+        password = findViewById(R.id.login_password);
+        btnLogin  = findViewById(R.id.login);
+        textRegister = findViewById(R.id.text_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+
+        textRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }        });    }
+
+
+    private void login()
+    {
+        String user = email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if(user.isEmpty())
+        {            email.setError("Email can not be empty");        }
+        if(pass.isEmpty())
+        {            password.setError("Password can not be empty");        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this , MainActivity.class));
+                    }
+                    else
+                    {
+                        Toast.makeText(LoginActivity.this, "Login Failed"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });        }    } }
