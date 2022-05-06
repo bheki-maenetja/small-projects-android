7abc5d571295808ac3551d5c1435b89c621a8ca2
Bheki Maenetja <bhekimaenetja@gmail.com>
2022-05-06 15:50:39 +0100

    Implemented functionality to allow users to view articles from original source

--- TheEarlyBird/app/src/main/java/com/example/theearlybird/DetailsActivity.java
+++ TheEarlyBird/app/src/main/java/com/example/theearlybird/DetailsActivity.java
@@ -2,7 +2,11 @@ package com.example.theearlybird;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +17,7 @@ public class DetailsActivity extends AppCompatActivity {
     Article headlines;
     TextView txt_title, txt_author, txt_time, txt_detail, txt_content;
     ImageView img_news;
+    Button externalLinkBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,7 @@ public class DetailsActivity extends AppCompatActivity {
         txt_detail = findViewById(R.id.text_detail_detail);
         txt_content = findViewById(R.id.text_detail_content);
         img_news = findViewById(R.id.img_detail_news);
+        externalLinkBtn = findViewById(R.id.externalLink);
 
         headlines = (Article) getIntent().getSerializableExtra("data");
 
@@ -34,5 +40,15 @@ public class DetailsActivity extends AppCompatActivity {
         txt_detail.setText(headlines.getDescription());
         txt_content.setText(headlines.getContent());
         Picasso.get().load(headlines.getUrlToImage()).into(img_news);
+        
+        externalLinkBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(headlines.getUrl()));
+                startActivity(i);
+            }
+        });
     }
 }
